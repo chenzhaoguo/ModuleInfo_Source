5222962e7acba12a64a94b0834c5e6295403aee9
chenlongxiang <chenlongxiang@indemind.cn>
2018-11-01 12:15:37 +0800

    source update

---
+++ cameraabout.cpp
@@ -0,0 +1,75 @@
+﻿#include "CameraAbout.h"
+
+CameraAbout::CameraAbout()
+{
+    //默认构造
+}
+//设置文件存放地址
+void CameraAbout::setFileSavePath(QString filePath){
+    imu_camera_file_path = filePath;
+}
+void CameraAbout::setCollectStateCamera(bool camera_state, QString time){
+    m_state_camera = camera_state;
+    if(time != NULL)
+        m_time = time;
+}
+//临时误差值存储
+double camera_time_temp_c = 0;
+void CameraAbout::slot_setCameraData(imrCameraData data){
+    if(m_state_camera){
+        QString cam0_data = imu_camera_file_path + QString::fromLocal8Bit("/cam0/data/");
+        QString cam1_data = imu_camera_file_path + QString::fromLocal8Bit("/cam1/data/");
+
+        QFile new_imu_data_0(imu_camera_file_path + QString::fromLocal8Bit("/cam0/data.csv"));
+        QFile new_imu_data_1(imu_camera_file_path + QString::fromLocal8Bit("/cam1/data.csv"));
+
+        // 检查目录是否存在，若不存在则新建
+        QDir dir;
+        if (!dir.exists(cam0_data))
+        {
+            dir.mkpath(cam0_data);
+        }
+        if (!dir.exists(cam1_data))
+        {
+            dir.mkpath(cam1_data);
+        }
+
+        if(!new_imu_data_0.open(QIODevice::Append | QIODevice::Text))
+        {
+            return;
+        }
+        if(!new_imu_data_1.open(QIODevice::Append | QIODevice::Text))
+        {
+            return;
+        }
+        //获取图像
+        m_image = new QImage(data._image,2560,800,QImage::Format_Indexed8);
+        //图片切割 左|右
+        QImage pic_1 = m_image->copy(0,0,1280,800);
+        QImage pic_2 = m_image->copy(1280,0,1280,800);
+        //保存图片
+        long long pic_d = basetime + data._timeStamp * 1000000;
+        //QString pic_d = QString("%1").arg(pic_n,0,'g',20);
+        pic_1.save(cam0_data + QString::number(pic_d) + ".png","PNG");
+        pic_2.save(cam1_data + QString::number(pic_d) + ".png","PNG");
+
+        QTextStream imu_data_0(&new_imu_data_0);
+        QTextStream imu_data_1(&new_imu_data_1);
+        //左右目数据是一致的
+        QString imu0 = QString::number(pic_d) + " " + QString::number(pic_d) + ".png";
+        imu_data_0<<imu0<<endl;
+        imu_data_1<<imu0<<endl;
+        new_imu_data_0.close();
+        new_imu_data_1.close();
+
+        if(m_image != NULL){
+            delete m_image;
+            m_image = NULL;
+        }
+    }
+}
+CameraAbout::~CameraAbout()
+{
+    //默认析构
+}
+
---
+++ imr_driver.h
@@ -0,0 +1,141 @@
+﻿#pragma once
+#include <stdint.h>
+#include "driver_data.h"
+
+#ifdef usbdriver_EXPORTS
+#ifdef WIN32
+#define _IMR_DRIVER_DLL_  __declspec( dllexport )
+#else
+#define _IMR_DRIVER_DLL_  __declspec( dllexport )
+#endif
+#else
+#ifdef WIN32
+#define _IMR_DRIVER_DLL_ __declspec( dllimport )
+#else
+#define _IMR_DRIVER_DLL_ __declspec( dllimport )
+#endif
+#endif
+typedef void* IMRDEVICE_HANDLE;
+
+#define IMR_ID_LENGTH 40
+
+//#ifdef __cplusplus
+//extern "C" {
+//#endif
+
+    /* 图像分辨率 */
+    typedef enum {
+        P752,
+        P640,
+    }Resolution;
+    /* 相机数据格式 */
+    typedef enum {
+        Mono8,
+        Mono10,
+    }CameraDataFormat;
+
+    typedef enum {
+        IMR_HMD,
+        IMR_CAMERA_LEFT,
+        IMR_CAMERA_RIGHT,
+        IMR_HAND_LEFT,
+        IMR_HAND_RIGHT,
+    }DeviceType;
+
+    //相机数据回调函数
+    typedef void(*CameraDataCallback)(imrCameraData* data);
+    /* 相机配置信息 */
+    typedef struct {
+        int _freq;
+        Resolution _res;
+        CameraDataFormat _format;
+        CameraDataCallback _cb;		//数据回调
+        void* _reserve;				//保留参数,可能定为相机补偿参数
+    }CameraConfig;
+
+    typedef struct {
+        char _id[IMR_ID_LENGTH];
+        DeviceType _type;   //设备的类型
+        bool _status;       //连接状态
+        void* _reserve;		//保留参数
+    }DeviceInfo;
+
+    typedef struct {
+        short devNum;           //当前的设备数量
+        DeviceInfo* _devInfo;   //每个设备的信息
+    }DevicesInfo;
+
+    typedef struct {
+        //手柄可视距离
+        //手柄可视角信息
+    }HandConfig;
+
+
+    /* 打开MR设备,请求所有设备信息和状态 */
+    _IMR_DRIVER_DLL_ IMRDEVICE_HANDLE imrOpenIMR();
+
+    /* 初始化相机 */
+    _IMR_DRIVER_DLL_ bool imrInitCamera(IMRDEVICE_HANDLE pHandle, CameraConfig& cfg);
+
+    /* 启用相机数据回调 */
+    _IMR_DRIVER_DLL_ bool imrStartCamera(IMRDEVICE_HANDLE pHandle);
+    /* 停止相机数据回调 */
+    _IMR_DRIVER_DLL_ void imrStopCamera(IMRDEVICE_HANDLE pHandle);
+
+    /* 检索有效的设备，返回设备信息 */
+    _IMR_DRIVER_DLL_ DevicesInfo imrSearchDevices(IMRDEVICE_HANDLE pHandle);
+
+    /* IMU数据回调函数 */
+    typedef void(*IMUDataCallback)(imrIMUData* data);
+    /* 连接设备,并设置IMU回调函数,对于没有IMU数据的设备,回调函数设置为NULL,该函数为同步函数,最多等待timeout
+     * 1. 手柄配对：判断对应ID的手柄是否处在长按状态,长按表示连接成功,返回true
+    */
+    _IMR_DRIVER_DLL_ bool imrConnectDevice(IMRDEVICE_HANDLE pHandle, const char* deviceID, int timeout, IMUDataCallback callback);
+
+    /* 查询设备当前状态，返回状态码,将状态码按位与得到具体状态 */
+    _IMR_DRIVER_DLL_ unsigned int imrGetDeviceStatus(IMRDEVICE_HANDLE pHandle, const char* deviceID);
+
+    /* 读取头显设备的标定等参数信息 */
+    _IMR_DRIVER_DLL_ bool imrReadHMDCalibration(IMRDEVICE_HANDLE pHandle, char** bin,size_t* len);
+
+    /* 写入头显设备的标定信息 */
+    _IMR_DRIVER_DLL_ int imrWriteHMDCalibration(IMRDEVICE_HANDLE pHandle, const char* bin, size_t len);
+
+    /* 读取手柄设备的标定等参数信息 */
+    _IMR_DRIVER_DLL_ bool imrReadHandCalibration(IMRDEVICE_HANDLE pHandle, char** bin, size_t* len, DeviceType);
+
+    /* 写入手柄设备的标定信息 */
+    _IMR_DRIVER_DLL_ bool imrWriteHandCalibration(IMRDEVICE_HANDLE pHandle, const char* bin, size_t len, DeviceType);
+
+    /* 查询设备电量 */
+    _IMR_DRIVER_DLL_ int imrGetDevicePower(IMRDEVICE_HANDLE pHandle, const char* deviceID);
+
+    /* 查询手柄电量 */
+    _IMR_DRIVER_DLL_ int imrGetControllerPower(IMRDEVICE_HANDLE pHandle, const char* deviceID);
+
+    /* 设置LED灯亮度, [0~7]盏灯,亮度0~255 */
+    _IMR_DRIVER_DLL_ int imrLightenLED(IMRDEVICE_HANDLE pHandle, char light[8]);
+
+    /* 低电量回调函数 */
+    typedef void(*LowPowerCallback)(imrPowerData* data);
+    /* 设置低电量回调函数 */
+    _IMR_DRIVER_DLL_ void imrSetLowPowerCallback(LowPowerCallback callback);
+
+    /* 设置手柄为省电模式 */
+    _IMR_DRIVER_DLL_ void imrPowerSavingMode(IMRDEVICE_HANDLE pHandle, DeviceType devType);
+
+    /* 头显插入拔出回调函数 */
+    typedef void(*HMDHotplugCallback)(bool bArrive);
+    /* 设置头显插入拔出回调函数 */
+    _IMR_DRIVER_DLL_ void imrSetHMDHotplugCallback(HMDHotplugCallback callback);
+
+    /* 手柄按键回调函数 */
+    typedef void(*KeyCallback)(imrInputData* data);
+    _IMR_DRIVER_DLL_ void imrSetInputCallback(IMRDEVICE_HANDLE pHandle, KeyCallback callback);
+
+    /* 关闭MR设备 */
+    _IMR_DRIVER_DLL_ void imrCloseIMR(IMRDEVICE_HANDLE pHandle);
+
+//#ifdef __cplusplus
+//}
+//#endif
